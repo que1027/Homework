4a99edbf23c9c985ae1e31c69934b13bcc8d1a51
Quinton Taylor <quintontaylor29@gmail.com>
2024-05-05 17:16:55 -0500

    week 4 HW

--- Week4/index.js
+++ Week4/index.js
@@ -1,2 +1,38 @@
 const titleElement = document.getElementById("title");
-console.log(titleElement.innerHTML);
+const bodyElement = document.getElementById("body");
+
+//Title
+titleElement.innerHTML = "Trip Doge";
+//header
+bodyElement.innerHTML =
+  "<header>" +
+  "<h1>Welcome To Trip Doge</h1>" +
+  "</header>" +
+  "<p>" +
+  "Welcome to Trip Doge, where we chronicle our journeys to inspire fellow travelers with ideas for their own adventures. Join us as we share tales of exploration, culinary discoveries, and cultural immersion to ignite your wanderlust and guide you on your next memorable trip." +
+  "<main>" +
+  "<h2>Illinois</h2>" +
+  "<ul>" +
+  "<li>Chicago</li>" +
+  '<img id="chipic" src="chi.jpg"/> <br />' +
+  "<span id='chispan'>" +
+  "Exploring Chicago, Illinois was a vibrant journey filled with diverse culinary delights, iconic landmarks, and natural wonders. Our adventure began with a savory stop at Harold's Chicken Shack, where we indulged in crispy, flavorful fried chicken that left our taste buds tingling with satisfaction. Next, we soared to new heights aboard the Ferris wheel at Navy Pier, marveling at the breathtaking views of the city skyline and Lake Michigan below. After our exhilarating ride, we ventured to the serene oasis of Garfield Park Conservatory, immersing ourselves in the lush greenery and vibrant blooms that transported us to a world of tranquility and beauty. To cap off our Chicago escapade, we cooled off with delectable gelato from Frio Gelato, conveniently located next to the Ferris wheel, savoring creamy scoops of Italian-inspired goodness that provided the perfect sweet finale to our unforgettable journey through the Windy City.</span>" +
+  "<li>Alton</li>" +
+  '<img id="altonpic"src="alton.jpg"/><br />' +
+  "</ul>" +
+  "<span id='altspan'>" +
+  "Embarking on a captivating voyage through Alton, Illinois, we uncovered a tapestry of history, myth, and culinary delights. Our first stop led us to the awe-inspiring Robert Wadlow Statue, a towering tribute to the world's tallest man, whose legacy looms large over the quaint town. Continuing our exploration, we ventured into the depths of legend at the Piasa Bird Cave, where ancient Native American artwork adorned the limestone cliffs, whispering tales of mythical creatures and ancient civilizations. As the sun dipped below the horizon, we found ourselves drawn to the lively ambiance of Fast Eddie's Bar and Grill, where the tantalizing aroma of sizzling burgers and cold drinks filled the air, offering a taste of Alton's vibrant nightlife and hospitality. In each moment, Alton revealed itself as a treasure trove of history, mystery, and culinary delights, leaving an indelible imprint on our journey through the heart of Illinois.</span>" +
+  "<h2>Missouri</h2>" +
+  "<ul>" +
+  "<li>Saint Louis</li>" +
+  '<img id="stlpic" src="stl.jpg"/>' +
+  "</ul>" +
+  "<span id='stlspan'>" +
+  "Our voyage through St. Louis, Missouri, was a symphony of cultural exploration and culinary delights against the backdrop of the majestic Gateway Arch. We commenced our adventure with a visit to the iconic St. Louis Cathedral Basilica, where the intricate mosaic artistry and awe-inspiring architecture spoke volumes of the city's rich religious heritage. Energized by our spiritual experience, we ventured into the heart of St. Louis's culinary scene, indulging in the unique flavors of Imo's Pizza, where the crispy thin crust and tangy Provel cheese left our taste buds longing for more. As the sun dipped low, casting a golden hue over the Mississippi River, we embraced the serenity of nature, casting our lines and immersing ourselves in the timeless tradition of fishing along its storied banks. St. Louis captivated us with its blend of history, gastronomy, and natural beauty, offering a glimpse into the soul of the Gateway to the West.</span>" +
+  "</p>" +
+  "</main>" +
+  "<a>" +
+  "<footer>© 2024 Travel Doge | Powered by Zax Tech " +
+  '<a href="Unavailible.html">Work With Travel Doge</a> | ' +
+  '<a href="Unavailible.html">Privacy Policy</a>' +
+  "</footer>";
